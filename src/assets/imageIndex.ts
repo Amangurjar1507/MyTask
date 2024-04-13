9c80a8341b089bd17fd4f8baf10eee64b63c5891
Amangurjar1507 <Amangurjar1507@gmail.com>
2024-04-13 19:53:11 +0530

    Add image

--- src/assets/imageIndex.ts
+++ src/assets/imageIndex.ts
@@ -1,16 +1,3 @@
 export default {
-  
-   Menu: require('./Menu.png'),
-  coppy: require('./coppy.png'),
-  notificatio: require('./notificatio.png'),
-  Ellipse: require('./Ellipse.png'),
-  card: require('./card.png'),
-  waring: require('./waring.png'),
-  MainCoin: require('./MainCoin.png'),
-
-
-  
+  swipeRight: require('./swipeRight.png'),
 };
-
-
- 
