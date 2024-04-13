beb737588164cbbf255e50ddc1daf187b28538a0
Amangurjar1507 <Amangurjar1507@gmail.com>
2024-04-13 19:45:47 +0530

    color file

--- src/theme/color.ts
+++ src/theme/color.ts
@@ -1,7 +1,8 @@
 const color = {
+  white: '#fff',
   black: '#000000',
-  red: '#FF4433',
-  buttonColor: '#EF4E86',
+  buttonColor: '#027C8A',
   textWhite: '#ffffff',
+  textGray:'#494949'
 };
 export default color;
