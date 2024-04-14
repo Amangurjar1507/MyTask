a4c2be45bf175d6e9a00d32040307ab4cbea29c4
Amangurjar1507 <Amangurjar1507@gmail.com>
2024-04-14 11:48:02 +0530

    Ur change

--- src/services/config/constant.ts
+++ src/services/config/constant.ts
@@ -1,3 +1,3 @@
 export default {
-   baseURL:"https://newsapi.org/v2/everything?q=tesla&from=2024-03-13&sortBy=publishedAt&apiKey=e8c7b1cfc077434da70e38e46ebe9474"
+   baseURL:"https://newsapi.org/v2/everything?q=tesla&from=2024-03-14&sortBy=publishedAt&apiKey=e8c7b1cfc077434da70e38e46ebe9474"
   };
